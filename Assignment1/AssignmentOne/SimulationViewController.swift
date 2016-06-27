8bad48391f9f90beb1081ea972cee54e23a4e4fc
rrballenger17 <rrb@alumni.stanford.edu>
2016-06-26 22:09:53 -0400

    drag and drop

---
+++ Assignment1/AssignmentOne/SimulationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AssignmentOne
+//
+//  Created by Ryan Ballenger on 6/25/16.
+//  Copyright © 2016 ios. All rights reserved.
+//
+
+import UIKit
+
+class SimulationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
